3219841265c6c87fff04276337ac915f54a637b7
bryanj408 <bryan.arndt52@gmail.com>
2024-02-20 19:59:29 -0800

    Created pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-let size = 100;
-let doubleSize = size * 2;
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
+//empty array
+pokemonList = [
+    { name: 'Bulbasaur', height: '0.7m', types: ['Grass', 'Poison']},
+    { name: 'Charmander', height: '0.6m', types: ['Fire']},
+    { name: 'Squirtle', height: '0.5m', types: ['Water']}
+];
