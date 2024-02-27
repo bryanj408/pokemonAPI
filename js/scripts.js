580500200eafc3c72a43d8bb2fc5dbdfa2e20635
bryanj408 <bryan.arndt52@gmail.com>
2024-02-27 07:55:49 -0800

    Modified loop with line break

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
 pokemonList = [
-    { name: 'Bulbasaur', height: 2, types: ['Grass', 'Poison']},
+    { name: 'Bulbasaur', height: 2, types: ['Grass', ' Poison']},
     { name: 'Charmander', height: 1, types: ['Fire']},
     { name: 'Squirtle', height: .5, types: ['Water']}
 ];
 
+//loops through pokemonList using a template literal to inject strings and variables
+//Added <br> to seperate each object. \n does not display correctly on the DOM
 for (let i = 0; i < pokemonList.length; i++) {
-    let list = `${pokemonList[i].name} (Height: ${pokemonList[i].height}) (Types: ${pokemonList[i].types})`;
-    document.write(list);
+    let result = `${pokemonList[i].name} (Height: ${pokemonList[i].height}) (Types: ${pokemonList[i].types})<br>`;
+    document.write(result);
 }
