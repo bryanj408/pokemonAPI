e8e0b704cf8c706bc5d2c2f32009ab6ba6452194
bryanj408 <bryan.arndt52@gmail.com>
2024-03-07 07:33:07 -0800

    Changed for loop to forEach
    commented out code - This is just for random practice

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,38 @@ pokemonList = [
 
 //loops through pokemonList using a template literal to inject strings and variables
 //Added <br> to seperate each object. \n does not display correctly on the DOM
-for (let i = 0; i < pokemonList.length; i++) {
-    let result = `${pokemonList[i].name} (Height: ${pokemonList[i].height}) (Types: ${pokemonList[i].types})<br>`;
+// for (let i = 0; i < pokemonList.length; i++) {
+//     let result = `${pokemonList[i].name} (Height: ${pokemonList[i].height}) (Types: ${pokemonList[i].types})<br>`;
+//     document.write(result);
+// }
+
+pokemonList.forEach((pokemon) => {
+    let result = `${pokemon.name} (Height: ${pokemon.height}) (Types: ${pokemon.types})<br>`;
     document.write(result);
-}
+})
+
+
+
+
+
+// let objectName = {
+//     name: "Joe",
+//     Age: 29,
+//     color: "Blue",
+//     emptyArray: []
+// }
+
+// //displays object keys
+// console.log(Object.keys(objectName));
+// //adds Anne to end of array
+// objectName.emptyArray.push("Anne");
+// console.log(objectName);
+// objectName.emptyArray.push("Bryan", "Haley");
+// //adds Marlowe to the beginning of array
+// objectName.emptyArray.unshift("Marlowe");
+// //removes last item of array
+// objectName.emptyArray.pop()
+// //removes first item of the array
+// objectName.emptyArray.shift()
+// //reverses items of the array
+// objectName.emptyArray.reverse();
